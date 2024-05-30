06d5d82208c0c21c9b1262b2fc6de9513f3ca2f1
Георгий <151121188+Nickixer@users.noreply.github.com>
2024-05-30 10:46:56 +0300

    Delete task15.jl

--- task15.jl
+++
@@ -1,9 +0,0 @@
-function task_15(r::Robot)
-    path = go_to_west_south_corner_and_return_path!(r; go_around_barriers = true)
-
-    for i ∈ (Nord, Ost, Sud, West)
-        go_to_border_and_return_path!(r, i; markers = true)
-    end
-    go_by_path!(r, path)
-    
-end
